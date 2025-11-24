34a0323ad28cd0af6aebc0407cb0f32a261a6823
Rezza-Web <3086901133@smkn4padalarang.sch.id>
2025-11-24 10:56:08 +0700

    update kabinet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ export default function Hero() {
   };
 
   return (
-    <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden">
+    <section className="relative min-h-screen flex items-center justify-center  sm:px-6 lg:px-8 overflow-hidden">
       {/* Background Efek Mouse */}
       <div
         className="absolute inset-0 opacity-30"
